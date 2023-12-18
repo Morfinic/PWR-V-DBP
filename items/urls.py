b47fdfb36906f13468fd7f63048ab3884bd5c464
morfinic@protonmail.com <a7Sm425*$%$9%%x@@#F#oH6*x>
2023-12-18 18:56:30 +0100

    Doano podstrone z opisem przedmiotu.

--- items/urls.py
+++ items/urls.py
@@ -5,4 +5,7 @@ app_name = 'items'
 
 urlpatterns = [
     path("item_list/", views.item_list, name="item_list"),
+    path("<int:pk>/", views.detail, name="detail"),
+    # path("<int:pk>/", views.detail, name="detail"),
+    # path("<int:pk>/", views.detail, name="detail"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Produkt
 from .forms import FilterForm
 
@@ -13,8 +13,7 @@ def item_list(request):
             ilosc__range=[form.data["ilosc_min"], form.data["ilosc_max"]],
             cena__range=[form.data["cena_min"], form.data["cena_max"]],
             wiek__range=[form.data["wiek_min"], form.data["wiek_max"]],
-        )
-        produkty.order_by(form.data["sortowanie"])
+        ).order_by(form.data["sortowanie"])
 
     else:
         form = FilterForm()
@@ -23,4 +22,12 @@ def item_list(request):
     return render(request, "item_list.html", {
         "form": form,
         "produkty": produkty
+    })
+
+
+def detail(request, pk):
+    produkt = get_object_or_404(Produkt, pk=pk)
+
+    return render(request, "detail.html", {
+        "produkt": produkt
     })
